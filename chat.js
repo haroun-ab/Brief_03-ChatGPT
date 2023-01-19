1a6820ec12b0ce0b1ac61463036063e2ff72b246
Colas Rabiller <colas.rabiller@gmail.com>
2023-01-19 17:35:01 +0100

    modif-main

--- chat.js
+++ chat.js
@@ -1,5 +1,3 @@
-
-
 //Effet hover input
 const input = document.querySelector("#input");
 let isFocus = "";
@@ -15,16 +13,16 @@ input.onblur = (e) => {
 
 // Question / Réponse
 const sendBtn = document.querySelector("#send-btn");
-const homeSection = document.querySelector("#home")
-const QAsection = document.createElement("section")
-document.querySelector('#top').appendChild(QAsection)
+const homeSection = document.querySelector("#home");
+const QAsection = document.createElement("section");
+document.querySelector("#top").appendChild(QAsection);
 
-QAsection.id = "qa-section"
+QAsection.id = "qa-section";
 
 // document.querySelector('#input-section').before(QAsection)
-let punctuation = 0
+let punctuation = 0;
 
-function randomPunctuation(){
+function randomPunctuation() {
   var punctuations = ["!", "?", "!!!", "?!", ".", "...", "?????", "!!!!!!!!!"];
   var random = Math.floor(Math.random() * punctuations.length);
   var randomValue = punctuations[random];
@@ -35,19 +33,16 @@ function randomPunctuation(){
 const tab = [
   { question: "Qui a toujours faim ?", answer: "Théo" },
   {
-    question: "Ma soeur avait la moitié de mon âge quand j'avais 20 ans. J'ai maintenant 40 ans. Quel âge a ma soeur ?",
+    question:
+      "Ma soeur avait la moitié de mon âge quand j'avais 20 ans. J'ai maintenant 40 ans. Quel âge a ma soeur ?",
     answer:
       "Si ta soeur avait la moitié de ton âge quand tu avais 20 ans, et que tu as maintenant 40 ans, elle a 30 ans. En effet : 20/2 + 20 = 30. ",
   },
   {
     question: "Quelle est la température du zéro absolu ?",
-    answer:
-     "Le zéro absolu correspond à -273,15 degrés celsius ou 0 kelvin.",
+    answer: "Le zéro absolu correspond à -273,15 degrés celsius ou 0 kelvin.",
   },
-  { question: "Qui a toujours une blague en tête ?",
-   answer:
-    "Vianney"
-  },
+  { question: "Qui a toujours une blague en tête ?", answer: "Vianney" },
 
   {
     question: "Quelle est la recette du tiramisu ?",
@@ -55,14 +50,16 @@ const tab = [
       "Pour 4 personnes, il faut 250 grammes de mascarpone, 100 grammes de sucre, 2 cuillères à soupe d'amaretto, 3 oeufs, 20 centilitres de café fort, 20 biscuits cuillères et de la poudre de cacao. Premièrement, fouettez 3 jaunes d'œufs avec le sucre jusqu'à ce que le mélange blanchisse et devienne mousseux. Ajoutez ensuite le mascarpone puis mélangez jusqu'à ce que le résultat soit bien homogène. Deuxièmement, Montez 3 blancs en neige ferme en y ajoutant une pincée de sel, puis mélangez-les à la crème au mascarpone. Mélangez délicatement de haut en bas avec une spatule en silicone. Troisièmement, Préparez une grande tasse de café fort et ajoutez-y l'amaretto. Quatrièmement, étalez une couche de crème au mascarpone au fond du plat, puis placez une couche de biscuits légèrement imbibés de café à l'amaretto.Pour finir, répétez l'opération en terminant par une couche de crème au mascarpone que vous saupoudrez de poudre de cacao amer ou de copeaux de chocolat. Placez au frais pour au moins 3 heures.",
   },
   {
-  question: "Envoie-moi un lien vers le top 10 des plus grands joueurs de foot de l'histoire.",
+    question:
+      "Envoie-moi un lien vers le top 10 des plus grands joueurs de foot de l'histoire.",
     answer:
       "Bien sûr, voici le lien que tu demandes : https://www.lequotidiendusport.fr/classement-les-dix-meilleurs-joueurs-de-tous-les-temps/",
   },
   {
-    question: "Quelle guerre a été la plus meurtrière entre la première et la deuxième guerre mondiale ?",
+    question:
+      "Quelle guerre a été la plus meurtrière entre la première et la deuxième guerre mondiale ?",
     answer:
-      "La Seconde Guerre mondiale fut le conflit le plus meurtrier de l'Histoire avec plus de 60 millions de morts soit 2,5% de la population mondiale de l'époque dont la majorité fut des civils. Les tableaux suivants détaillent les pertes de chaque pays.Globalement, les estimations des historiens varient de 50 millions à 85 millions de morts. Ces valeurs ne prennent pas en considération les morts de carences, privations, de suites de blessures, ou autres maladies consécutives au conflit, après mai 1945, en Europe, et septembre 1945, en Asie. ",
+      "La Seconde Guerre mondiale fut le conflit le plus meurtrier de l'Histoire avec plus de 60 millions de morts soit 2,5% de la population mondiale de l'époque dont la majorité fut des civils.Globalement, les estimations des historiens varient de 50 millions à 85 millions de morts. Ces valeurs ne prennent pas en considération les morts de carences, privations, de suites de blessures, ou autres maladies consécutives au conflit, après mai 1945, en Europe, et septembre 1945, en Asie. Les pertes humaines de la Première Guerre mondiale s'élèvent à environ 18,6 millions de morts. Ce nombre inclut 9,7 millions de morts pour les militaires et 8,9 millions pour les civils.",
   },
   {
     question: "Comment calculer l'hypoténuse ?",
@@ -76,7 +73,7 @@ const tab = [
   },
 ];
 
-console.log(tab.length)
+console.log(tab.length);
 
 function chatAnswer(e) {
   e.preventDefault();
@@ -100,38 +97,37 @@ function chatAnswer(e) {
   QAsection.appendChild(answerDiv);
 
   // Groot
-  if (grootBox.checked) {  
-    randomPunctuation()
+  if (grootBox.checked) {
+    randomPunctuation();
     if (!input.value == "") {
-    questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${input.value}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
-    answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="groot${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
+      questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${input.value}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
+      answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="groot${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
       var typewriter = new Typewriter(`#groot${numberDiv}`);
-    typewriter
-      .changeDelay(90)
-      .typeString(`Je suis Groot ${punctuation}`)
-      .pauseFor(2500)
-      .start();
+      typewriter
+        .changeDelay(90)
+        .typeString(`Je suis Groot ${punctuation}`)
+        .pauseFor(2500)
+        .start();
     } else {
       questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p  class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">...</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
       answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="groot${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
       var typewriter = new Typewriter(`#groot${numberDiv}`);
-    typewriter
-      .changeDelay(90)
-      .typeString(`Je suis Groot ${punctuation}`)
-      .pauseFor(2500)
-      .start();
+      typewriter
+        .changeDelay(90)
+        .typeString(`Je suis Groot ${punctuation}`)
+        .pauseFor(2500)
+        .start();
     }
-    input.value = ""
+    input.value = "";
   } else {
-  if (!input.value == "") 
-  {
-    console.log(input);
-    for (let i = 0; i < tab.length; i++) {
-      console.log(tab[i].question)
-      if (tab[i].question == input.value) {
-        questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${tab[i].question}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
-        answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="answer${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
-        // Typewriter
+    if (!input.value == "") {
+      console.log(input);
+      for (let i = 0; i < tab.length; i++) {
+        console.log(tab[i].question);
+        if (tab[i].question == input.value) {
+          questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${tab[i].question}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
+          answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="answer${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
+          // Typewriter
           var typewriter = new Typewriter(`#answer${numberDiv}`);
 
           typewriter
@@ -139,16 +135,17 @@ function chatAnswer(e) {
             .typeString(tab[i].answer)
             .pauseFor(2500)
             .start();
-        break;
-      } else {
-        questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${input.value}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
-        answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="sorry${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
-         var typewriter = new Typewriter(`#sorry${numberDiv}`);
+          break;
+        } else {
+          questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${input.value}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
+          answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="sorry${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
+          var typewriter = new Typewriter(`#sorry${numberDiv}`);
           typewriter
             .changeDelay(90)
             .typeString("Désolé, je ne sais pas répondre à votre question.")
             .pauseFor(2500)
             .start();
+        }
       }
     } else {
       questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">...</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
@@ -161,19 +158,7 @@ function chatAnswer(e) {
         .start();
     }
 
-input.value = "";
-  } else {
-    questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">...</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
-    answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6" id="empty${numberDiv}"></p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
-   var typewriter = new Typewriter(`#empty${numberDiv}`);
-      typewriter
-        .changeDelay(90)
-        .typeString("Vous n'êtes pas très bavard...")
-        .pauseFor(2500)
-        .start();
-    }
+    input.value = "";
   }
-}
-
 }
 sendBtn.addEventListener("click", chatAnswer);
