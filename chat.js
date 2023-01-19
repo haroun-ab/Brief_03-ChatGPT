b46ff59d967a132b2a74c7f98d4c1ef2d717c524
Haroun <hrnabd@hotmail.com>
2023-01-19 14:25:02 +0100

    commit before merging on main

--- chat.js
+++ chat.js
@@ -23,7 +23,7 @@ QAsection.id = "qa-section"
 // document.querySelector('#input-section').before(QAsection)
 let punctuation = 0
 
-function randomV(){
+function randomPunctuation(){
   var punctuations = ["!", "?", "!!!", "?!", ".", "...", "?????", "!!!!!!!!!"];
   var random = Math.floor(Math.random() * punctuations.length);
   var randomValue = punctuations[random];
@@ -94,7 +94,7 @@ function chatAnswer(e) {
 
   // Groot
   if (grootBox.checked) {  
-    randomV()
+    randomPunctuation()
     if (!input.value == "") {
     questionDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://i.pinimg.com/474x/46/72/f8/4672f876389036583190d93a71aa6cb2.jpg"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${input.value}</p><div class="col-2 col-sm-1 d-flex m-0 justify-content-start text-white-50 py-4 fs-7 ps-2"><i class="bi bi-pencil-square"></i></div>`;
     answerDiv.innerHTML = `<div class="col-1 d-flex justify-content-end m-0 py-4"><img class="rounded-1" height="25px" width="25px" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4~c5_720x720.jpeg?x-expires=1674032400&x-signature=Xo7N9Nq8b6oKPLexshs1XorTUSI%3D"/></div><p class="col-9 col-sm-10 px-4 px-lg-5 py-4 m-0 mt-1 fs-6">${"Je s'appelle groot" + punctuation}</p><div class="col-2 col-sm-1 d-flex justify-content-start text-white-50 py-4"><i class="bi bi-hand-thumbs-up p-0 p-sm-1"></i><i class="bi bi-hand-thumbs-down p-0 p-sm-1 ps-1"></i></div>`;
